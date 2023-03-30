c5e0e33ec390772046a3271eef70262ac5e21770
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-03-30 12:14:51 +0600

    ema john update and end

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const taxString = (total * 0.1).toFixed(2);
+    const tax = parseFloat(taxString);
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: $ {total}</p>
+            <p>Total Shipping: $ {shipping}</p>
+            <p>Tax: $ {tax}</p>
+            <h4>Grand Total: $ {grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
