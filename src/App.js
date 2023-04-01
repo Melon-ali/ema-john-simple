eebfadefd9db139fa44784f0392b7ccf97902a50
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-04-01 12:49:36 +0600

    shopping orders added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
+import Main from './layouts/Main';
+import Orders from './components/Orders/Orders';
+import About from './components/About/About';
 import Shop from './components/Shop/Shop';
+import Inventory from './components/Inventory/Inventory';
+import { productsAndCartLoader } from './loaders/productsAndCartLoader';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('products.json'),
+          element: <Shop></Shop>
+        },
+        {
+          path: '/orders',
+          loader: () => productsAndCartLoader(),
+          element: <Orders></Orders>
+        },
+        {
+          path: '/inventory',
+          element: <Inventory></Inventory>
+        },
+        {
+          path: 'about',
+          element: <About></About>
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <Header></Header>
-      <Shop></Shop>
+    <div>
+        <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+const ReviewItem = ({product, handleRemoveItem}) => {
+    const {id, name, price, quantity, img, shipping} = product;
+    return (
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-details-container">
+                <div className="review-details">
+                    <p>{name}</p>
+                    <p>Price:<span className='or-color'>${price}</span></p>
+                    <p>Shipping:<span className='or-color'>${shipping}</span></p>
+                    <p><small>Quantity: {quantity}</small></p>
+                </div>
+                <div className="delete-container">
+                    <button onClick={() => handleRemoveItem(id)} className='btn-delete'>
+                        <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewItem;
